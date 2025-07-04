f4a9fc994863d0d146a7d3b4c3e40311dcbe7c45
Sophia934 <sophia.prestes.silva@escola.pr.gov.br>
2025-07-04 09:59:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+
+criacartao(
+'Programação',
+'O que é Javascript?',
+'O javascript é uma linguagem de programção'
+)
+
+criacartao(
+'Geografia',
+'Qual é a capital do Paraná',
+'A capital do Paraná é Curitiba'
+)
+
+criacartao(
+'Programação',
+'O que é Python?',
+'O Python é uma linguagem de programção'
+)
